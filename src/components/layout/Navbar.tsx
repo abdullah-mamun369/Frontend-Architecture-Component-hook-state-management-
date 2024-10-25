a0f3ff64200ca0704eebdd31b06a2212313efee8
abdullah-mamun369 <maamamun369@gmail.com>
2024-10-25 23:10:03 +0600

    first commit

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>
+                This is the navigation
+            </h1>
+        </div>
+    );
+};
+
+export default Navbar; <h1>
+    This is the navigation</h1>
